31480655d4a415e23f1a8b5c6da018b00c694130
Nick Howard <ndh@baroquebobcat.com>
2010-10-08 08:22:23 -0600

    more specing + quick and dirty scripted version

--- cardgame_spec.rb
+++ cardgame_spec.rb
@@ -1,14 +1,24 @@
 module MountainRB
-  class CardGameHand
-    attr_accessor :cards
 
-    def initialize cards
-      @cards = cards
+  module CardGame
+    class Hand
+      attr_accessor :cards
+
+      def initialize cards
+        @cards = cards
+      end
+    end
+    class Card
+      attr_accessor :name, :type, :value
+      def initialize name, value
+        @name = name
+        @value =  value
+      end
     end
   end
 end
-describe MountainRB::CardGameHand do
-  it "takes a list of cards" do
-    MountainRB::CardGameHand.new []
+describe MountainRB::CardGame::Hand do
+  it "takes a list of 6 cards" do
+    MountainRB::CardGame::Hand.new 6.times.map{ MountainRB::CardGame::Card.new 'foo',1}
   end
 end
