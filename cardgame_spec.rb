77f6ab997d7e49cfe6cd08e266e04251cdb49113
Nick Howard <ndh@baroquebobcat.com>
2010-10-07 20:56:19 -0600

    continuing

--- cardgame_spec.rb
+++ cardgame_spec.rb
@@ -1,6 +1,14 @@
 module MountainRB
   class CardGameHand
+    attr_accessor :cards
+
+    def initialize cards
+      @cards = cards
+    end
   end
 end
 describe MountainRB::CardGameHand do
+  it "takes a list of cards" do
+    MountainRB::CardGameHand.new []
+  end
 end
