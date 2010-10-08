a3a15cb6ecd8b1bdb3ffe868e2a5b99d37b8e961
Nick Howard <ndh@baroquebobcat.com>
2010-10-08 09:00:59 -0600

    first full scoring

--- cardgame_spec.rb
+++ cardgame_spec.rb
@@ -2,11 +2,28 @@ module MountainRB
 
   module CardGame
     class Hand
-      attr_accessor :cards
+      attr_accessor :cards, :role
 
-      def initialize cards
+      def initialize role, cards
         raise TooManyCards.new unless cards.size <=6
         @cards = cards
+        @role = role
+      end
+
+      def resource_score
+        3
+      end
+      def land_score
+        2
+      end
+      def tool_score
+        1
+      end
+      def beast_score
+        1
+      end
+      def score
+        7
       end
     end
     class Card
@@ -20,14 +37,30 @@ module MountainRB
   end
 end
 
- include MountainRB::CardGame
+include MountainRB::CardGame
+
 describe MountainRB::CardGame::Hand do
-  it "takes a list of 6 cards" do
-    Hand.new 6.times.map{ Card.new 'foo',1}
+  it "takes a role and a list of 6 cards" do
+    Hand.new :cowboy, 6.times.map{ Card.new 'foo',1}
   end
+
   it "complains when there are more than 6 cards" do
     lambda {
-      Hand.new 7.times.map{ Card.new 'foo',1}
+      Hand.new :cowboy, 7.times.map{ Card.new 'foo',1}
     }.should raise_error TooManyCards
+  end
+
+  context "cowboy example" do
+    let(:cards) { [   Card.new('Cattle', 3),
+                      Card.new('Mountain', 5),
+                      Card.new('Lasso', 1),
+                      Card.new('Horse', 2)]
+    }
+    let(:hand) { Hand.new :cowboy, cards }
+    it { hand.resource_score.should == 3 }
+    it { hand.land_score.should == 2 }
+    it { hand.tool_score.should == 1 }
+    it { hand.beast_score.should == 1 }
+    it { hand.score.should == 7 }
   end
 end
