bccfe3f98faae62edd014a9e871c9f9351f4f4ae
Nick Howard <ndh@baroquebobcat.com>
2010-10-08 11:08:58 -0600

    some spec refactoring

--- cardgame_spec.rb
+++ cardgame_spec.rb
@@ -11,74 +11,94 @@ describe MountainRB::CardGame::Hand do
       Hand.new :cowboy, 7.times.map{ Card.new 'foo',1}
     }.should raise_error TooManyCards
   end
-
-  context "cowboy example 1 " do
-    let(:cards) { [   Card.new('Cattle', 3),
+  context "role as cowboy" do
+    let (:role) {:cowboy}
+    
+    context "example 1 " do
+      let(:cards) { [ Card.new('Cattle', 3),
                       Card.new('Mountain', 5),
                       Card.new('Lasso', 1),
-                      Card.new('Horse', 2)
-                  ] }
-    let(:hand) { Hand.new :cowboy, cards }
+                      Card.new('Horse', 2) ] }
+      let(:hand) { Hand.new role, cards }
+      
+      it { hand.resource_score.should == 3 }
+      it { hand.land_score.should == 2 }
+      it { hand.tool_score.should == 1 }
+      it { hand.beast_score.should == 1 }
+      it { hand.score.should == 7 }
+    end
 
-    it { hand.resource_score.should == 3 }
-    it { hand.land_score.should == 2 }
-    it { hand.tool_score.should == 1 }
-    it { hand.beast_score.should == 1 }
-    it { hand.score.should == 7 }
-  end
+    context "with only plains" do
+      let(:cards) { [ Card.new('Plains', 5) ] }
+      let(:hand) { Hand.new role, cards }
+      it { hand.land_score.should == 5 }
+    end
+    
+    context "with only mountains" do
+      let(:hand) { Hand.new role, [ Card.new('Mountain', 5) ] }
+      it { hand.land_score.should == 2 }
+    end
+    
+    context "with only forest" do
+      let(:hand) { Hand.new role, [ Card.new('Forest', 5) ] }
+      it { hand.land_score.should == 0 }
+    end
+    
+    context "cowboy with different cattle" do
+      let(:hand) { Hand.new role, [ Card.new('Horse', 8), Card.new('Cattle', 8) ] }
+      
+      it { hand.resource_score.should == 8 }
+    end
+    
+    context "with two lassos" do
+      let(:hand) { Hand.new role, [ Card.new('Lasso', 1), Card.new('Lasso', 8) ] }
+      
+      it { hand.tool_score.should == 8 }
+    end
 
-  context "cowboy with only plains" do
-    let(:cards) { [ Card.new('Plains', 5) ] }
-    let(:hand) { Hand.new :cowboy, cards }
-    it { hand.land_score.should == 5 }
-  end
-
-  context "cowboy with only mountains" do
-    let(:hand) { Hand.new :cowboy, [ Card.new('Mountain', 5) ] }
-    it { hand.land_score.should == 2 }
-  end
-
-  context "cowboy with only forest" do
-    let(:hand) { Hand.new :cowboy, [ Card.new('Forest', 5) ] }
-    it { hand.land_score.should == 0 }
-  end
-
-  context "cowboy with different cattle" do
-    let(:hand) { Hand.new :cowboy, [ Card.new('Horse', 8), Card.new('Cattle', 8) ] }
-
-    it { hand.resource_score.should == 8 }
-  end
-
- context "cowboy with two lassos" do
-    let(:hand) { Hand.new :cowboy, [ Card.new('Lasso', 1), Card.new('Lasso', 8) ] }
-
-    it { hand.tool_score.should == 8 }
-  end
-
-  context "cowboy with no horse" do
-    let(:cards) { [   Card.new('Cattle', 8),
+    context "with no horse" do
+      let(:cards) { [   Card.new('Cattle', 8),
                       Card.new('Mountain', 5),
                       Card.new('Lasso', 1)
                   ] }
-    let(:hand) { Hand.new :cowboy, cards }
-
-    it { hand.resource_score.should == 0 }
-    it { hand.beast_score.should == 0 }
+      let(:hand) { Hand.new role, cards }
+      
+      it { hand.resource_score.should == 0 }
+      it { hand.beast_score.should == 0 }
+    end
   end
 
-  context "prospector example" do
-    let(:cards) { [ Card.new('Ore Vein', 1),
-                    Card.new('Ore Vein', 5),
-                    Card.new('Mountain', 3),
-                    Card.new('Pickaxe', 4),
+  context "with role prospector" do
+    let(:role){:prospector}
+    context "example" do
+      let(:cards) { [ Card.new('Ore Vein', 1),
+                      Card.new('Ore Vein', 5),
+                      Card.new('Mountain', 3),
+                      Card.new('Pickaxe', 4),
                     Card.new('Burro', 2)
-                  ] }
-    let(:hand) { Hand.new :prospector, cards }
+                    ] }
+      let(:hand) { Hand.new role, cards }
+      
+      it { hand.resource_score.should == 6 }
+      it { hand.land_score.should == 3 }
+      it { hand.tool_score.should == 4 }
+      it { hand.beast_score.should == 4 }
+      it { hand.score.should == 17 }
+    end
 
-    it { hand.resource_score.should == 6 }
-    it { hand.land_score.should == 3 }
-    it { hand.tool_score.should == 4 }
-    it { hand.beast_score.should == 4 }
-#    it { hand.score.should == 17 }
+    context "with only mountains" do
+      let(:hand) { Hand.new role, [ Card.new('Mountain', 5) ] }
+      it { hand.land_score.should == 5 }
+    end
+    
+    context "with only forest" do
+      let(:hand) { Hand.new role, [ Card.new('Forest', 5) ] }
+      it { hand.land_score.should == 2 }
+    end
+    
+    context "with only plains" do
+      let(:hand) { Hand.new role, [ Card.new('Plains', 5) ] }
+      it { hand.land_score.should == 0 }
+    end
   end
 end
